0a9ef3b9f0b2f3fa8cdcf773292337330e719ef8
Sidney Losbanes <johnsidney88@gmail.com>
2017-02-22 18:52:03 +0800

    version
    Signed-off-by: Sidney Losbanes <johnsidney88@gmail.com>

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+    public static void main (String [] args) {
+       int x = 1;
+
+        while (x <= 10) {
+            System.out.print("["+x+"]");
+            x++;
+        }
+        
+    }
+    }
+
